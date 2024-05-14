426e80445ac8dad29961956c67e5454ad2ce1777
shane <6874@holbertonstudents.com>
2024-05-14 17:44:43 +0200

    task 9

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,22 @@
+function countPrimeNumbers() {
+	let total = 0;
+	let detected;
+	for ( let i = 2 ; i <= 100 ; i++) {
+		detected = false;
+		for (let x = 2 ; x < Math.sqrt(i) ; x++) {
+			if (i % x === 0) {
+				detected = true;
+				break;
+			}
+		}
+		if (detected === false) {
+			total += 1;
+		}
+	}
+	return total;
+}
+let start = performance.now();
+countPrimeNumbers();
+let end = performance.now();
+let time = end - start;
+console.log(`Execution time of printing countPrimeNumbers was ${time} milliseconds.`)
